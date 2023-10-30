ec01c84664f1563ef4fde7a38f37c2e33799ab96
nikita13122001 <113139821+nikita13122001@users.noreply.github.com>
2023-10-30 15:42:41 +0530

    Add files via upload

---
+++ js/resp.js
@@ -0,0 +1,13 @@
+burger=document.querySelector('.burger')
+navbar=document.querySelector('.navbar')
+rightNav=document.querySelector('.rightNav')
+navList=document.querySelector('.navList')
+
+
+
+burger.addEventListener('click',()=>{
+    rightNav.classList.toggle('v-class');
+    navList.classList.toggle('v-class');
+    navbar.classList.toggle('h-class');
+
+})
